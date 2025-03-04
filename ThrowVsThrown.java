0cb636fb2aa0978f91fa95455df90b971f08160b
Damini-Girase <girase.damini234@gmail.com>
2025-03-04 22:02:10 +0530

    Throw Vs Thrown

--- ThrowVsThrown.java
+++ ThrowVsThrown.java
@@ -1,15 +1,36 @@
+class NegitiveRadiusException extends Exception{
+    @Override
+    public String toString() {  
+        return "Radius cannot be negative!";
+    }
+    @Override
+    public String getMessage() {
+        return "Radius cannot be negative!";
+    }
+}
 public class ThrowVsThrown {
+    public static double area(int r) throws NegitiveRadiusException{
+        if(r<0){
+            throw new NegitiveRadiusException();
+        }
+        double result = Math.PI * r * r;
+        return result;
+    }
     public static int Divide(int a, int b) throws ArithmeticException{
        int result = a/b;
        return result;
     }
     public static void main(String[] args) {
         try{
-           int c = Divide(3,0);
-           System.out.println(c);
+        //    int c = Divide(3,0);
+        //    System.out.println(c);
+           double sc = area(7);
+           System.out.println(sc);
         }
         catch(Exception e){
             System.out.println("Exception");
+
+            
         }
     }
 }
