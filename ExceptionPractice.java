2039aef2afe9953d7813391edca929ed6b2c35a6
Damini-Girase <girase.damini234@gmail.com>
2025-03-05 23:31:37 +0530

    Exception practice

---
+++ ExceptionPractice.java
@@ -0,0 +1,88 @@
+
+// Practice problem 01
+
+//public class ExceptionPractice {
+  //  public static void main(String[] args) {
+        // Syntax error
+
+    //   int a = 12
+    //   System.out.println(a);
+
+      // Logical Error   --> We want print odd numbers
+
+    //   int n = 10;
+    //   for(int i = 0;i<=n;i++){
+    //     if(i%2==0){
+    //         System.out.println(i);
+    //     } 
+    //   }
+
+    // Runtime Error
+
+    // int [] marks = new int[2];
+    // System.out.println(marks[5]);
+
+
+  //  }
+//}
+ 
+// Practice problem 02
+
+// public class ExceptionPractice {
+// public static void great(){
+//     try{
+//         int a = 34;
+//         int b = 0;
+//         System.out.println(a/b);
+//     }
+//     catch(ArithmeticException e){
+//         System.out.println(" HAHA ArithmaticException");
+//     }
+// }
+// public static void goat(int age){
+//   if(age<0){
+//     throw new IllegalArgumentException("HIHI");
+//   }
+// }
+//     public static void main(String[] args) {
+//        great();
+//        ExceptionPractice sc = new ExceptionPractice();
+//        sc.goat(-2);
+//     }
+// }
+
+// Practice Problem 03
+
+// import java.util.Scanner;
+
+public class ExceptionPractice {
+    public static void main(String[] args) {
+       
+       
+    //     Scanner sc = new Scanner(System.in);
+    //     int [] marks = new int[5];
+    //     marks[0] = 98;
+    //     marks[1] = 99;
+    //     marks[2] = 94;
+    //     marks[3] = 95;
+    //     marks[4] = 90;
+    //     boolean flag = true;
+    //     int i = 0;
+    //     while (flag && i<5) {
+    //     try{
+    //         System.out.println("Enter array index: ");
+    //         int num = sc.nextInt();
+    //       System.out.println(marks[num]);
+    //       flag = false;
+    //     }
+    //     catch(ArrayIndexOutOfBoundsException e){
+    //         System.out.println("Out of Boundary index!");
+    //         i++;
+    //     }
+    // }
+    // if(i>=5){
+    //     System.out.println("Error");
+    // }
+    }
+}
+
