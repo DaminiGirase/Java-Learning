4a8766e7454d58232c6c08393540114f17a12065
Damini-Girase <girase.damini234@gmail.com>
2025-03-08 00:36:03 +0530

    Array Deque

--- Deque.java
+++ Deque.java
@@ -9,13 +9,13 @@ public class Deque {
         sc.addFirst(23);
         sc.addLast(56);
         sc.contains(9);
-    //     System.out.println(sc.isEmpty());
+    //System.out.println(sc.isEmpty());
     // System.out.println(sc.iterator());
     // System.out.println(sc.offer(9));
     // System.out.println(sc.offerFirst(3));
     // System.out.println(sc.offerLast(67));
     //System.out.println(sc.peek());
-   // System.out.println(sc.poll());
+    // System.out.println(sc.poll());
    
    //System.out.println(sc.pop());
        System.out.println(sc.clone()); 
